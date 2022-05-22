e7ca0a0ead76d5617d8e00f05ea3e7f8ce7bbcb4
Glenn Meyer <grossmeyer@gmail.com>
2022-05-22 00:07:06 -0400

    Added twitter links to contact section

--- pages/index.js
+++ pages/index.js
@@ -18,8 +18,11 @@ export default function Home() {
               Contact
             </h2>
             <hr className='mb-5 w-1/6' />
-            <p className='text-sm hover:text-sky-300'><a href='mailto:glenn@glennmeyer.dev'>glenn@glennmeyer.dev</a></p>
-            <a href='https://www.linkedin.com/in/glenn-meyer-93a58583/' aria-label='Connect with Glenn on LinkedIn'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className='mt-1'><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg></a>
+            <p className='md:text-sm text-lg hover:text-sky-300 mb-2'><a href='mailto:glenn@glennmeyer.dev'>glenn@glennmeyer.dev</a></p>
+            <section className='flex flex-row'>
+              <a href='https://www.linkedin.com/in/glenn-meyer-93a58583/' aria-label='Connect with Glenn on LinkedIn'><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" className='mt-1 mr-4 hover:bg-sky-300 rounded-xl'><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg></a>
+              <a href='https://www.twitter.com/grossmeyer' aria-label='Connect with Glenn on Twitter'><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" className='mt-1 mr-4 hover:bg-sky-300 rounded-xl'><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-.139 9.237c.209 4.617-3.234 9.765-9.33 9.765-1.854 0-3.579-.543-5.032-1.475 1.742.205 3.48-.278 4.86-1.359-1.437-.027-2.649-.976-3.066-2.28.515.098 1.021.069 1.482-.056-1.579-.317-2.668-1.739-2.633-3.26.442.246.949.394 1.486.411-1.461-.977-1.875-2.907-1.016-4.383 1.619 1.986 4.038 3.293 6.766 3.43-.479-2.053 1.08-4.03 3.199-4.03.943 0 1.797.398 2.395 1.037.748-.147 1.451-.42 2.086-.796-.246.767-.766 1.41-1.443 1.816.664-.08 1.297-.256 1.885-.517-.439.656-.996 1.234-1.639 1.697z" /></svg></a>
+            </section>
           </section>
           {/* Education Section */}
           <section className='text-slate-100'>
