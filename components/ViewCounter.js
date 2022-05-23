e7c8b7262fdcb8c40614460b8bdbc83706c131f2
Glenn Meyer <grossmeyer@gmail.com>
2022-05-22 21:31:17 -0400

    Created ViewCounter with fake data

---
+++ components/ViewCounter.js
@@ -0,0 +1,33 @@
+import { useRouter } from 'next/router'
+
+export default function ViewCounter() {
+  const db = {
+    "PAGE#/": {
+      "viewCount": 100
+    },
+    "PAGE#/about": {
+      "viewCount": 101
+    }
+  }
+  const router = useRouter()
+  const page = router.pathname
+
+  console.log('Page is', page)
+
+  const updateViewCount = page => {
+    const pk = db[`PAGE#${page}`]
+    pk.viewCount++
+    const vcStr = pk.viewCount.toString().padStart(5, '0')
+    return vcStr
+  }
+
+  const viewCount = updateViewCount(page)
+
+  return (
+    <footer className='flex flex-col font-main text-center items-center'>
+      <div className='mt-4 p-2 text-lg text-slate-100 bg-sky-700 rounded-lg w-1/6'>
+        <p>Page views: {viewCount}</p>
+      </div>
+    </footer>
+  )
+}
