b786baa94a9db0aa9b363221e0066d11534a5c01
Glenn Meyer <grossmeyer@gmail.com>
2022-07-23 15:53:47 -0400

    Fixed mobile navlink highlighting

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,6 +6,8 @@ export default function Navbar() {
   const router = useRouter()
   const activePage = 'flex py-4 px-2 text-sky-700 font-semibold'
   const otherPage = 'flex py-4 px-2 text-slate-600 hover:text-sky-600 font-semibold transition duration-300'
+  const mobileActive = 'flex justify-center py-2 mb-2 text-md bg-sky-900 text-sky-500 w-1/3 rounded-md font-semibold'
+  const mobileOther = 'flex justify-center py-2 mb-2 text-md text-white bg-sky-500 w-1/3 rounded-md font-semibold'
   useEffect(() => {
     const button = document.querySelector('button.mobile-menu-button')
     const menu = document.querySelector('.mobile-menu')
@@ -22,13 +24,16 @@ export default function Navbar() {
           {/* Logo */}
           <section className='flex items-center py-4 px-2'>
             <img src='avatar.webp' alt='GlennMeyer.DEV Logo' width='32' height='35' className='w-8 mr-2' />
-            <span className='font-semibold text-sky-700 text-lg'>GlennMeyer.dev</span>
+            <span className='font-semibold text-sky-700 text-lg'>GLENN MEYER</span>
           </section>
           {/* Main Nav Items */}
           <section className='flex'>
             <div className='hidden md:flex items-center space-x-7'>
               <Link href='/'>
                 <a className={router.pathname == '/' ? activePage : otherPage}>Home</a>
+              </Link>
+              <Link href='/resume' >
+                <a className={router.pathname == '/resume' ? activePage : otherPage}>Resume</a>
               </Link>
               <Link href='/about' >
                 <a className={router.pathname == '/about' ? activePage : otherPage}>About</a>
@@ -56,8 +61,9 @@ export default function Navbar() {
       <section>
         <div className='hidden mobile-menu'>
           <ul className='flex flex-col items-center'>
-            <li className='flex justify-center py-2 mb-2 text-md text-white bg-sky-500 w-1/3 rounded-md font-semibold'><Link href='/' className='block'>Home</Link></li>
-            <li className='flex justify-center py-2 mb-2 text-md text-white bg-sky-500 w-1/3 rounded-md font-semibold'><Link href='/about' className='block'>About</Link></li>
+            <li className={router.pathname == '/' ? mobileActive : mobileOther}><Link href='/'>Home</Link></li>
+            <li className={router.pathname == '/resume' ? mobileActive : mobileOther}><Link href='/resume'>Resume</Link></li>
+            <li className={router.pathname == '/about' ? mobileActive : mobileOther}><Link href='/about'>About</Link></li>
           </ul>
         </div>
       </section>
